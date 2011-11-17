5ded69fd73bc7e05f3227448934873a2eaadcf5b
Tian Hu <tianhu114@gmail.com>
2011-11-17 01:01:49 -0500

    add new files

---
+++ src/js/znode/global.js
@@ -0,0 +1,17 @@
+  var win = $(window);
+  var paper;
+  var canvas;
+  var overlay;
+  var mouseX = 0, mouseY = 0;
+  var loops = [];    
+  var currentNode;  
+  var currentConnection = {};
+  var connections = {};  
+  var newNode;  
+  var nodes = {};
+  var topHeight;
+  
+  function showOverlay(){
+    overlay.show();
+    overlay.css({"width" : win.width(), "height" : win.height()}); //, "opacity": 0.1});
+  }
---
+++ src/js/znode/nodebase.js
@@ -0,0 +1,186 @@
+ function NodeBase(xp, yp, w, h, noDelete, forceId){
+    var nId; 	
+	var zindex = 1;
+    
+    if (forceId){
+       nId = forceId;
+    }
+    this.id = nId;
+    
+    var curr = this;
+
+    
+    canvas.append("<div class='node'/>");
+    var n = $(".node").last();
+    n.css({"position" : "absolute",      
+           "left" : xp, "top" : yp,
+           "width" : w, "height" : h,   
+           "border" : "1px solid gray",
+           "background-color" : "white"});
+    n.css("z-index", zindex++);
+           
+    this.content = n;
+    
+    this.width = function(){
+      return n.width();
+    }
+    this.height = function(){
+      return n.height();
+    }
+    this.x = function(){
+      return n.position().left;
+    }
+    this.y = function(){
+      return n.position().top;
+    }
+         
+    var nodeWidth = n.width();
+    var nodeHeight = n.height();
+           
+    n.append("<div class='bar'/>");
+    var bar = $(".node .bar").last();
+    bar.css({"height" : "20px", 
+             "background-color" : "gray", 
+             "padding" : "0", "margin": "0",
+             "font-size" : "9px", "cursor" : "pointer"});
+             
+             
+    if (!noDelete){
+      n.append("<div class='ex'>X<\/div>");
+      var ex = $(".node .ex").last();
+      ex.css({"position":"absolute","padding-right" : 2, "padding-top" : 1, "padding-left" : 2,
+              "color" : "white", "font-family" : "sans-serif",
+              "top" : 0, "left": 0, "cursor": "pointer",
+              "font-size" : "7px", "background-color" : "gray", "z-index" : 100});
+      ex.hover(function(){
+        ex.css("color","black");
+      }, function(){
+        ex.css("color","white");
+      }).click(function(){
+      
+        if (confirm("Are you sure you want to delete this node?")){
+          curr.remove();
+        }
+      });
+    }
+   
+    n.append("<textarea class='txt' spellcheck='false' />");
+    var txt = $(".node .txt").last();
+    txt.css("position","absolute");
+   
+    txt.css({"width" : nodeWidth - 5,
+             "height" : nodeHeight - bar.height() - 5,
+             "resize" : "none", "overflow" : "hidden",
+             "font-size" : "12px" , "font-family" : "sans-serif",
+             "border" : "none","z-index":4});
+          
+    this.txt = txt;
+    
+    n.append("<div class='resizer' />");
+    var resizer = $(".node .resizer").last();
+    
+    resizer.css({"position" : "absolute" , "z-index" : 10,
+                 "width" : "10px", "height" : "10px",
+                 "left" : nodeWidth - 11, "top" : nodeHeight - 11,
+                 "background-color" : "white", "font-size" : "1px",
+                 "border" : "1px solid gray",
+                 "cursor" : "pointer"});
+		
+	
+    
+    n.append("<div class='left'>");
+    n.append("<div class='top'>");
+    n.append("<div class='right'>");
+    n.append("<div class='bottom'>");
+    
+    var left = $(".node .left").last();
+    left.css("left","-11px");
+    
+    var top = $(".node .top").last();
+    top.css("top","-11px");
+    
+    var right = $(".node .right").last();
+    var bottom = $(".node .bottom").last();
+    
+    positionLeft();
+    positionRight();
+    positionTop();
+    positionBottom();
+    
+    this.left = left;
+    this.right = right;
+    this.top = top;
+    this.bottom = bottom;
+    
+    function positionLeft(){
+      left.css("top", n.height() / 2 - 5);
+    }
+    function positionRight(){
+      right.css("left",n.width() + 1).css("top", n.height() / 2 - 5);
+    }
+    function positionTop(){
+      top.css("left", n.width() / 2 - 5);
+    }
+    function positionBottom(){
+      bottom.css("top",n.height() + 1).css("left", n.width() / 2 - 5);
+    }    
+  
+   this.remove = function(){
+     n.remove();
+     delete nodes[this.id];
+   }
+    
+    resizer.mousedown(function(e){
+      currentNode = curr;
+      e.preventDefault();
+      startDrag(resizer, {left : 60, top : 40, right : 500, bottom : 500},
+      function(){
+        var loc = resizer.position();
+        var x = loc.left;
+        var y = loc.top;
+        n.css({"width" : x + resizer.width() + 1,
+               "height" : y + resizer.height() + 1});
+        
+        txt.css({"width" : n.width() - 5, "height" : n.height() - bar.height() - 5});
+        
+        positionLeft();
+        positionRight();
+        positionTop();
+        positionBottom();
+      });
+    });
+	
+    
+    bar.mousedown(function(e){
+      currentNode = curr;
+      n.css("z-index", zindex++);
+      e.preventDefault();
+      startDrag(n, {left : 10, top: 40, right : win.width() - n.width() - 10, bottom : win.height() - n.height() - 10});
+    });
+    
+    n.mouseenter(function(){
+      n.css("z-index", zindex++);
+    });
+	
+	function startDrag(element, bounds, dragCallback){
+    showOverlay();	
+    var startX = mouseX - element.position().left;
+    var startY = mouseY - element.position().top;
+    if (!dragCallback) dragCallback = function(){};
+      var id = setInterval(function(){
+      var x = mouseX - startX;
+      var y = mouseY - startY;
+      if (bounds){
+        if (x < bounds.left) x = bounds.left;
+        if (x > bounds.right) x = bounds.right;
+        if (y < bounds.top) y = bounds.top;
+        if (y > bounds.bottom) y = bounds.bottom;
+      }
+      element.css("left", x).css("top",y);
+      dragCallback();
+    },topHeight);
+    loops.push(id);
+  }
+
+ }
+ 
