f96547383f8ff17ff615c876a479360d5f32501d
Tian Hu <tianhu114@gmail.com>
2011-11-17 00:59:04 -0500

    Updated UI

--- src/js/znode/nodegraph.js
+++ src/js/znode/nodegraph.js
@@ -1,26 +1,17 @@
-function NodeGraph(){
-  var win = $(window);
-  var canvas = $("#canvas");
-  var overlay = $("#overlay");
-  var currentNode;
-  var currentConnection = {};
-  var connections = {};
+function NodeGraph(){  
   var connectionId = 0;
-  var newNode;
-  var nodes = {};
-  var nodeId = 0;
-  var mouseX = 0, mouseY = 0;
-  var loops = [];
+  var nodeId = 0;  
   var pathEnd = {};
-  var zindex = 1;
   var hitConnect;
   var key = {};
   var SHIFT = 16;
   var defaultWidth = 150;
-  var defaultHeight = 50;
-  var topHeight = $("#controls").height();
+  var defaultHeight = 50;  
   
-  var paper = new Raphael("canvas", "100", "100");
+  canvas = $("#canvas");
+  overlay = $("#overlay");
+  topHeight = $("#controls").height();
+  paper = new Raphael("canvas", "100", "100");
   
   function resizePaper(){
     paper.setSize(win.width(), win.height() - topHeight);
@@ -53,8 +44,6 @@ function NodeGraph(){
     var node, x, y;
     dir = dir.toLowerCase();
     
-    
-    
       
     if (dir == "left"){
       x = pathEnd.x + 5;
@@ -72,7 +61,7 @@ function NodeGraph(){
     }
     
  
-    node = new Node(x, y, currentNode.width(), currentNode.height());
+    node = addNode(x, y, currentNode.width(), currentNode.height());
     saveConnection(node, dir);
     currentNode = node;
   }
@@ -210,322 +199,6 @@ function NodeGraph(){
             height : function(){ return c.height(); }};
   }
   
-  function showOverlay(){
-    overlay.show();
-    overlay.css({"width" : win.width(), "height" : win.height()}); //, "opacity": 0.1});
-  }
-  
-  function startDrag(element, bounds, dragCallback){
-    showOverlay();
-    var startX = mouseX - element.position().left;
-    var startY = mouseY - element.position().top;
-    if (!dragCallback) dragCallback = function(){};
-      var id = setInterval(function(){
-      var x = mouseX - startX;
-      var y = mouseY - startY;
-      if (bounds){
-        if (x < bounds.left) x = bounds.left;
-        if (x > bounds.right) x = bounds.right;
-        if (y < bounds.top) y = bounds.top;
-        if (y > bounds.bottom) y = bounds.bottom;
-      }
-      element.css("left", x).css("top",y);
-      dragCallback();
-    },topHeight);
-    loops.push(id);
-  }
-  
-  
-  function Node(xp, yp, w, h, noDelete, forceId){
-    
-    if (forceId){
-       nodeId = forceId;
-    }
-    this.id = nodeId;
-    nodes[nodeId] = this;
-    nodeId++;
-    
-    var curr = this;
-    this.connections = {};
-    var connectionIndex = 0;
-    
-    this.addConnection = function(c){
-      curr.connections[connectionIndex++] = c;
-      return c;
-    }
-    
-    canvas.append("<div class='node'/>");
-    var n = $(".node").last();
-    n.css({"position" : "absolute",      
-           "left" : xp, "top" : yp,
-           "width" : w, "height" : h,   
-           "border" : "1px solid gray",
-           "background-color" : "white"});
-    n.css("z-index", zindex++);
-           
-    this.content = n;
-    
-    this.width = function(){
-      return n.width();
-    }
-    this.height = function(){
-      return n.height();
-    }
-    this.x = function(){
-      return n.position().left;
-    }
-    this.y = function(){
-      return n.position().top;
-    }
-         
-    var nodeWidth = n.width();
-    var nodeHeight = n.height();
-           
-    n.append("<div class='bar'/>");
-    var bar = $(".node .bar").last();
-    bar.css({"height" : "20px", 
-             "background-color" : "gray", 
-             "padding" : "0", "margin": "0",
-             "font-size" : "9px", "cursor" : "pointer"});
-             
-             
-    if (!noDelete){
-      n.append("<div class='ex'>X<\/div>");
-      var ex = $(".node .ex").last();
-      ex.css({"position":"absolute","padding-right" : 2, "padding-top" : 1, "padding-left" : 2,
-              "color" : "white", "font-family" : "sans-serif",
-              "top" : 0, "left": 0, "cursor": "pointer",
-              "font-size" : "7px", "background-color" : "gray", "z-index" : 100});
-      ex.hover(function(){
-        ex.css("color","black");
-      }, function(){
-        ex.css("color","white");
-      }).click(function(){
-      
-        if (confirm("Are you sure you want to delete this node?")){
-          curr.remove();
-        }
-      });
-    }
-   
-    n.append("<textarea class='txt' spellcheck='false' />");
-    var txt = $(".node .txt").last();
-    txt.css("position","absolute");
-   
-    txt.css({"width" : nodeWidth - 5,
-             "height" : nodeHeight - bar.height() - 5,
-             "resize" : "none", "overflow" : "hidden",
-             "font-size" : "12px" , "font-family" : "sans-serif",
-             "border" : "none","z-index":4});
-          
-    this.txt = txt;
-    
-    n.append("<div class='resizer' />");
-    var resizer = $(".node .resizer").last();
-    
-    resizer.css({"position" : "absolute" , "z-index" : 10,
-                 "width" : "10px", "height" : "10px",
-                 "left" : nodeWidth - 11, "top" : nodeHeight - 11,
-                 "background-color" : "white", "font-size" : "1px",
-                 "border" : "1px solid gray",
-                 "cursor" : "pointer"});
-		
-	n.append("<div class='sourcecode'>SRC");		 
-	var sourcecode = $(".node .sourcecode").last();
-    
-    sourcecode.css({"position" : "absolute" , "z-index" : 10,
-                 "width" : "30px", "height" : "15px",
-                 "left" : nodeWidth + 8 - n.width() , "top" : nodeHeight - n.height(),
-                 "font-size" : "10px",
-				 "font-family": "Arial", "text-align": "center",
-                 "border" : "1px solid gray",				 
-				 "border-radius": "5px",
-				 "box-shadow": "inset 0 35px 35px -18px #95B9C7, inset 3px 0 12px #600",
-				 "background-color": "white",
-                 "cursor" : "pointer"});
-				 
-    sourcecode.hover(function(){
-        sourcecode.css("background-color","gray")},
-		function(){
-        sourcecode.css("background-color","white");
-      });
-	  
-	n.append("<div class='variable'>VAR");		 
-	var variable = $(".node .variable").last();
-    
-    variable.css({"position" : "absolute" , "z-index" : 10,
-                 "width" : "30px", "height" : "15px",
-                 "left" : nodeWidth + 40 - n.width() , "top" : nodeHeight - n.height(),
-                 "font-size" : "10px",
-				 "font-family": "Arial", "text-align": "center",
-                 "border" : "1px solid gray",
-				 "border-radius": "5px",
-				 "box-shadow": "inset 0 35px 35px -18px #95B9C7, inset 3px 0 12px #600",
-				 "background-color": "white",
-                 "cursor" : "pointer"});
-				 
-    variable.hover(function(){
-        variable.css("background-color","gray")},
-		function(){
-        variable.css("background-color","white");
-      });
-    
-    n.append("<div class='left'>");
-    n.append("<div class='top'>");
-    n.append("<div class='right'>");
-    n.append("<div class='bottom'>");
-    
-    var left = $(".node .left").last();
-    left.css("left","-11px");
-    
-    var top = $(".node .top").last();
-    top.css("top","-11px");
-    
-    var right = $(".node .right").last();
-    var bottom = $(".node .bottom").last();
-    
-    setupConnection(left);
-    setupConnection(right);
-    setupConnection(top);
-    setupConnection(bottom);
-    
-    positionLeft();
-    positionRight();
-    positionTop();
-    positionBottom();
-    
-    this.left = left;
-    this.right = right;
-    this.top = top;
-    this.bottom = bottom;
-    
-    function positionLeft(){
-      left.css("top", n.height() / 2 - 5);
-    }
-    function positionRight(){
-      right.css("left",n.width() + 1).css("top", n.height() / 2 - 5);
-    }
-    function positionTop(){
-      top.css("left", n.width() / 2 - 5);
-    }
-    function positionBottom(){
-      bottom.css("top",n.height() + 1).css("left", n.width() / 2 - 5);
-    }
-    
-    function setupConnection(div){
-      div.css({"position" : "absolute", "width" : "10px", "padding":0,
-               "height" : "10px", "background-color" : "#aaaaaa",
-               "font-size" : "1px", "cursor" : "pointer"});
-    }
-    
-    this.connectionPos = function(conn){
-      var loc = conn.position();
-      var nLoc = n.position();
-      var point = {};
-      point.x = nLoc.left + loc.left + 5;
-      point.y = nLoc.top - topHeight + loc.top + 5;
-      return point;
-    }
-    
-    function updateConnections(){
-       for (var i in curr.connections){
-         var c = curr.connections[i];
-         if (!c.removed){
-           var nodeA = c.startNode.connectionPos(c.startConnection);
-           var nodeB = c.endNode.connectionPos(c.endConnection);
-           c.attr("path","M " + nodeA.x + " " + nodeA.y + " L " + nodeB.x + " " + nodeB.y);
-            
-         }
-       }
-    }
-    this.updateConnections = updateConnections;
-    
-    
-   function addLink(e){
-      currentNode = curr;
-      e.preventDefault();
-      showOverlay();
-      var link = paper.path("M 0 0 L 1 1");
-      link.attr({"stroke-width":2});
-      currentConnection = link;
-      currentConnection.parent = $(this);
-      
-      curr.addConnection(link);
-      var loc = $(this).position();
-      var nLoc = n.position();
-      var x = loc.left + nLoc.left + 5;
-      var y = loc.top + nLoc.top - topHeight + 5;
-      newNode = true;
-      
-      var id = setInterval(function(){
-        link.attr("path","M " + x + " " + y + " L " + mouseX + " " + mouseY);
-        
-        pathEnd.x = mouseX;
-        pathEnd.y = mouseY;
-      }, 30);
-      loops.push(id);
-   }
-   left.mousedown(addLink);
-   right.mousedown(addLink);
-   top.mousedown(addLink);
-   bottom.mousedown(addLink);
-   
-   this.remove = function(){
-     for (var i in curr.connections){
-       var c = curr.connections[i];
-       c.remove();
-       delete connections[c.id];
-       delete curr.connections[i];
-     }
-     n.remove();
-     delete nodes[this.id];
-   }
-    
-    resizer.mousedown(function(e){
-      currentNode = curr;
-      e.preventDefault();
-      startDrag(resizer, {left : 60, top : 40, right : 500, bottom : 500},
-      function(){
-        var loc = resizer.position();
-        var x = loc.left;
-        var y = loc.top;
-        n.css({"width" : x + resizer.width() + 1,
-               "height" : y + resizer.height() + 1});
-        
-        txt.css({"width" : n.width() - 5, "height" : n.height() - bar.height() - 5});
-        
-        positionLeft();
-        positionRight();
-        positionTop();
-        positionBottom();
-        updateConnections();
-      });
-    });
-	
-	sourcecode.mousedown(function(e){
-      var openWin = $("#textbox2");
-	  openWin.toggle();
-    });
-	
-    variable.mousedown(function(e){
-      var openWin = $("#textbox1");
-	  openWin.toggle();
-    });
-    
-    bar.mousedown(function(e){
-      currentNode = curr;
-      n.css("z-index", zindex++);
-      e.preventDefault();
-      startDrag(n, {left : 10, top: 40, right : win.width() - n.width() - 10, bottom : win.height() - n.height() - 10},
-      updateConnections);
-    });
-    
-    n.mouseenter(function(){
-      n.css("z-index", zindex++);
-    });
-    
-  }
-  
   function hitTest(a, b){
     var aPos = a.position();
     var bPos = b.position();
@@ -548,7 +221,6 @@ function NodeGraph(){
       );
   }
   
-  
  function clear(){
     nodeId = 0;
     connectionsId = 0;
@@ -564,21 +236,23 @@ function NodeGraph(){
     currenNode = null;
   }
   
-  this.addNode = function(x, y, w, h, noDelete){
-    return new Node(x, y, w, h, noDelete);
+  this.addNode = function(xp, yp, w, h, noDelete, forceId){  
+    var node = new Node(xp, yp, w, h, noDelete, forceId);
+	nodes[nodeId] = node;
+    nodeId++;
+    return node; 
   }
   
   this.addNodeAtMouse = function(){
     //alert("Zevan");
     var w = currentNode.width() || defaultWidth;
     var h = currentNode.height () || defaultHeight;
-    var temp = new Node(mouseX, mouseY + 30, w, h);
+    var temp = this.addNode(mouseX, mouseY + 30, w, h);
     currentNode = temp;
     currentConnection = null;
   }
   
-  function defaultNode(){
-    
+  function defaultNode(){  
     var temp = new Node(win.width() / 2 - defaultWidth / 2, 
                         win.height() / 2 - defaultHeight / 2,
                         defaultWidth, defaultHeight, true);
@@ -642,4 +316,5 @@ function NodeGraph(){
     str = str.replace(/\n/g,'\\\\n');
     return str;
   }
+
 }
